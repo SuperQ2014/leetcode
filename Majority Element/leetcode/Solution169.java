034954eea131b106cff4b9e4afb80f53e474e0f2
chaoqiang <tao-chqang@163.com>
2016-04-07 21:39:21 +0800

    add Majority Element using Bit Manipulation

---
+++ Majority Element/leetcode/Solution169.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class Solution169 {
+
+	public int majorityElement(int[] nums) {
+		int result = 0;
+		for (int i = 0; i < 32; i++) {
+			int zeros = 0;
+			int ones = 0;
+			for (int j = 0; j < nums.length; j++) {
+				if ((nums[j] & (1 << i)) != 0) {
+					++ones;
+				} else {
+					++zeros;
+				}
+				
+				if (ones > zeros)
+					result |= (1 << i);
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Solution169 mySolution = new Solution169();
+		int[] nums = {1,1,1,2,2};
+		int result = mySolution.majorityElement(nums);
+		System.out.println(result);
+	}
+}
